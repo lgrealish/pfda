2d16ad15e405a034881829c961301ee1227492bb
Linda Grealish <lindagrealish@hotmail.com>
2023-10-12 11:07:39 +0100

    Added intro files

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,14 @@
 
-print("hello world")
+import datetime
+
+print("Check if it's Tuesday")
+
+today = datetime.datetime.today()
+dayOfWeek = today.weekday()
+
+if dayOfWeek == 1:
+  print("It is Tuesday")
+elif dayOfWeek == 3:
+  print("It's Wednesday")
+else: 
+  print("It's not Tuesday")
